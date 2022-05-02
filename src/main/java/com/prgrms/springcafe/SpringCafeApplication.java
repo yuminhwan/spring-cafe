613e36a63dcf6c906eb295f94e9620c278d21b8a
yuminhwan <ymh7532@icloud.com>
2022-05-02 17:53:33 +0900

    chore: 프로젝트 초기 설정

---
+++ src/main/java/com/prgrms/springcafe/SpringCafeApplication.java
@@ -0,0 +1,13 @@
+package com.prgrms.springcafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCafeApplication.class, args);
+	}
+
+}
