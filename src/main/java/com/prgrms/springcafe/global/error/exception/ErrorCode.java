760e7aa4c81fe97c7fdad5ab4c0c7f005a4ade0f
yuminhwan <ymh7532@icloud.com>
2022-05-03 19:03:30 +0900

    feat: EntityNotFoundException 구현

--- src/main/java/com/prgrms/springcafe/global/error/exception/ErrorCode.java
+++ src/main/java/com/prgrms/springcafe/global/error/exception/ErrorCode.java
@@ -1,7 +1,8 @@
 package com.prgrms.springcafe.global.error.exception;
 
 public enum ErrorCode {
-    INVALID_INPUT_VALUE(400, "잘못된 값 입력입니다.");
+    INVALID_INPUT_VALUE(400, "잘못된 값 입력입니다."),
+    ENTITY_NOT_FOUND(400, "엔티티를 찾을 수 없습니다.");
 
     private final int status;
     private final String message;
